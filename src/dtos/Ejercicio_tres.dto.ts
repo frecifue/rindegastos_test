0b14ab3f5d78b605f878adf56e2fd14a26b6c0c1
fredy cifuentes <fcifuentes@ubiobio.cl>
2024-07-05 11:11:09 -0400

    mejora dto

--- src/dtos/Ejercicio_tres.dto.ts
+++ src/dtos/Ejercicio_tres.dto.ts
@@ -1,16 +1,18 @@
 import { Type } from "class-transformer";
-import { IsNotEmpty, IsNumber, IsNumberString, IsPositive } from "class-validator";
+import { IsInt, IsNotEmpty, IsNumber, IsNumberString, IsPositive } from "class-validator";
 
 export class EjercicioTresDto{
     @IsNumber()
     @IsNotEmpty()
     @IsPositive()
+    @IsInt()
     @Type(() => Number)
     readonly first: number;
 
     @IsNumber()
     @IsNotEmpty()
     @IsPositive()
+    @IsInt()
     @Type(() => Number)
     readonly second: number;
 }
